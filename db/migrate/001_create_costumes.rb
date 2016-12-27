5dd18a2035eab19f722f0e7a2a75a074e566d208
Derek Kaknes <derek.kaknes@gmail.com>
2016-12-27 21:57:37 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,10 +11,11 @@ class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :size
       t.string :image_url
-      t.timestamps
+      
+      t.timestamps null: false
     end
   end
 end
